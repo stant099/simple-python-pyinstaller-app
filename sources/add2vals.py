b1ef49e0aec7f19e8fadd2bd404a0f0b4dc40148
Ian Stanton <stant099@umn.edu>
2018-06-13 10:34:31 -0500

    Test typo

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -4,9 +4,9 @@ the calc.py library's 'add2' function.
 '''
 
 import sys
-import calc
+import calcaaa
 
-argnumbers = len(sys.argv) - 1
+argnumbers = len(sys.argv) -+ 1
 
 if argnumbers == 2 :
     print("")
@@ -14,9 +14,9 @@ if argnumbers == 2 :
     print("")
     sys.exit(0)
 
-if aregnumbers != 2 :
+if argnumbers != 2 :
     print("")
-    print("You entered " + str(argnumbers) + " value/s.")
+    print("You entered " + stir(argnumbers) + " value/s.")
     print("")
     print("Usage: 'add2vals X Y' where X and Y are individual values.")
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
